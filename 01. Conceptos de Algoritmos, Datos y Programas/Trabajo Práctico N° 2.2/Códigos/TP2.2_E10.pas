761a3bfb11536cf907059ea5694a573886bbc80e
Juan Menduiña <97914935+menduinajuan@users.noreply.github.com>
2025-03-04 00:34:43 -0300

    Commit

--- 01. Conceptos de Algoritmos, Datos y Programas/Trabajo Práctico N° 2.2/Códigos/TP2.2_E10.pas
+++ 01. Conceptos de Algoritmos, Datos y Programas/Trabajo Práctico N° 2.2/Códigos/TP2.2_E10.pas
@@ -9,6 +9,74 @@ Nota: En caso de no cumplir, informar qué parte del patrón no se cumplió.}
 program TP2_E10;
 {$codepage UTF8}
 uses crt;
+const
+  caracter_corte1='$'; caracter_corte2='#';
+function es_vocal(c: char): boolean;
+begin
+  es_vocal:=(c='A') or (c='E') or (c='I') or (c='O') or (c='U') or (c='a') or (c='e') or (c='i') or (c='o') or (c='u');
+end;
+function cumple_secuenciaA(cumple: boolean): boolean;
+var
+  c: char;
+begin
+  textcolor(green); writeln('Introducir caracter de la secuencia A:');
+  textcolor(yellow); readln(c);
+  while ((c<>caracter_corte1) and (cumple=true)) do
+  begin
+    if (es_vocal(c)=false) then
+      cumple:=false
+    else
+    begin
+      textcolor(yellow); readln(c);
+    end;
+  end;
+  cumple_secuenciaA:=cumple;
+end;
+function cumple_secuenciaB(cumple: boolean): boolean;
+var
+  c: char;
+begin
+  textcolor(green); writeln('Introducir caracter de la secuencia B:');
+  textcolor(yellow); readln(c);
+  while ((c<>caracter_corte2) and (cumple=true)) do
+  begin
+    if (es_vocal(c)=true) then
+      cumple:=false
+    else
+    begin
+      textcolor(yellow); readln(c);
+    end;
+  end;
+  cumple_secuenciaB:=cumple;
+end;
+var
+  cumple: boolean;
+begin
+  cumple:=true;
+  cumple:=cumple_secuenciaA(cumple);
+  if (cumple=true) then
+  begin
+    cumple:=cumple_secuenciaB(cumple);
+    if (cumple=true) then
+    begin
+      textcolor(red); write('La secuencia cumple con el patrón A$B#');
+    end
+    else
+    begin
+      textcolor(red); write('La secuencia no cumple con la parte B del patrón A$B#');
+    end;
+  end
+  else
+  begin
+    textcolor(red); write('La secuencia no cumple con la parte A del patrón A$B#');
+  end;
+end.
+
+{program TP2_E10;}
+{$codepage UTF8}
+{uses crt;
+const
+  caracter_corte1='$'; caracter_corte2='#';
 function leer_secuencia(secuencia: string): string;
 begin
   textcolor(green); write('Introducir secuencia de caracteres: ');
@@ -19,47 +87,45 @@ function es_vocal(c: char): boolean;
 begin
   es_vocal:=(c='A') or (c='E') or (c='I') or (c='O') or (c='U') or (c='a') or (c='e') or (c='i') or (c='o') or (c='u');
 end;
-procedure parseo_string(var cumple_A, cumple_B, cumple_AB: boolean);
+procedure parseo_string(var cumple_A, cumple_B: boolean);
 var
-  i, j: int8;
+  i: int8;
   secuencia: string;
 begin
   secuencia:='';
   secuencia:=leer_secuencia(secuencia);
   i:=1;
-  while (secuencia[i]<>'$') do
+  while (secuencia[i]<>caracter_corte1) do
   begin
     cumple_A:=cumple_A and (es_vocal(secuencia[i])=true);
-    cumple_AB:=cumple_AB and (es_vocal(secuencia[i])=true);
     i:=i+1;
   end;
-  j:=i+1;
-  while (secuencia[j]<>'#') do
+  i:=i+1;
+  while (secuencia[i]<>caracter_corte2) do
   begin
-    cumple_B:=cumple_B and (es_vocal(secuencia[j])=false);
-    cumple_AB:=cumple_AB and (es_vocal(secuencia[j])=false);
-    j:=j+1;
+    cumple_B:=cumple_B and (es_vocal(secuencia[i])=false);
+    i:=i+1;
   end;
 end;
 var
-  cumple_A, cumple_B, cumple_AB: boolean;
+  cumple_A, cumple_B: boolean;
 begin
-  cumple_A:=true; cumple_B:=true; cumple_AB:=true;
-  parseo_string(cumple_A,cumple_B,cumple_AB);
-  if (cumple_AB=true) then
+  cumple_A:=true; cumple_B:=true;
+  parseo_string(cumple_A,cumple_B);
+  if ((cumple_A=true) and (cumple_B=true)) then
   begin
-    textcolor(yellow); write('La secuencia cumple con el patrón A$B#');
+    textcolor(red); write('La secuencia cumple con el patrón A$B#');
   end
   else if ((cumple_A=false) and (cumple_B=true)) then
   begin
-    textcolor(yellow); write('La secuencia no cumple con la parte A del patrón A$B#');
+    textcolor(red); write('La secuencia no cumple con la parte A del patrón A$B#');
   end
   else if ((cumple_A=true) and (cumple_B=false)) then
   begin
-    textcolor(yellow); write('La secuencia no cumple con la parte B del patrón A$B#');
+    textcolor(red); write('La secuencia no cumple con la parte B del patrón A$B#');
   end
   else
   begin
-    textcolor(yellow); write('La secuencia no cumple con las partes A y B del patrón A$B#');
+    textcolor(red); write('La secuencia no cumple con las partes A y B del patrón A$B#');
   end;
-end.
+end.}
--- 01. Conceptos de Algoritmos, Datos y Programas/Trabajo Práctico N° 2.2/Códigos/TP2.2_E11.pas
+++ 01. Conceptos de Algoritmos, Datos y Programas/Trabajo Práctico N° 2.2/Códigos/TP2.2_E11.pas
@@ -10,58 +10,127 @@ program TP2_E11;
 {$codepage UTF8}
 uses crt;
 const
-  arrobas_corte=3;
+  caracter_corte1='%'; caracter_corte2='*';
+  caracter_corte3='$';
+  caracter_corte4='@'; cantidad_caracter_corte4=3;
+procedure cumple_secuenciaA(var cumple: boolean; var caracteresA: int8);
+var
+  c: char;
+begin
+  textcolor(green); writeln('Introducir caracter de la secuencia A:');
+  textcolor(yellow); readln(c);
+  while ((c<>caracter_corte1) and (cumple=true)) do
+  begin
+    if (c=caracter_corte3) then
+      cumple:=false
+    else
+    begin
+      caracteresA:=caracteresA+1;
+      textcolor(yellow); readln(c);
+    end;
+  end;
+end;
+procedure cumple_secuenciaB(var cumple: boolean; caracteresA: int8);
+var
+  caracteresB, caracteres_corte4: int8;
+  c: char;
+begin
+  caracteresB:=0; caracteres_corte4:=0;
+  textcolor(green); writeln('Introducir caracter de la secuencia B:');
+  textcolor(yellow); readln(c);
+  while ((c<>caracter_corte2) and (cumple=true)) do
+  begin
+    caracteresB:=caracteresB+1; 
+    if (c=caracter_corte4) then
+      caracteres_corte4:=caracteres_corte4+1;
+    if ((caracteresB>caracteresA) or (caracteres_corte4>cantidad_caracter_corte4)) then
+      cumple:=false
+    else
+    begin
+      textcolor(yellow); readln(c);
+    end;
+  end;
+end;
+var
+  cumple: boolean;
+  caracteresA: int8;
+begin
+  cumple:=true; caracteresA:=0;
+  cumple_secuenciaA(cumple,caracteresA);
+  if (cumple=true) then
+  begin
+    cumple_secuenciaB(cumple,caracteresA);
+    if (cumple=true) then
+    begin
+      textcolor(red); write('La secuencia cumple con el patrón A%B*');
+    end
+    else
+    begin
+      textcolor(red); write('La secuencia no cumple con la parte B del patrón A%B*');
+    end;
+  end
+  else
+  begin
+    textcolor(red); write('La secuencia no cumple con la parte A del patrón A%B*');
+  end;
+end.
+
+{program TP2_E11;}
+{$codepage UTF8}
+{uses crt;
+const
+  caracter_corte1='%'; caracter_corte2='*';
+  caracter_corte3='$';
+  caracter_corte4='@'; cantidad_caracter_corte4=3;
 function leer_secuencia(secuencia: string): string;
 begin
   textcolor(green); write('Introducir secuencia de caracteres: ');
   textcolor(yellow); readln(secuencia);
   leer_secuencia:=secuencia;
 end;
-procedure parseo_string(var cumple_A, cumple_B, cumple_AB: boolean);
+procedure parseo_string(var cumple_A, cumple_B: boolean);
 var
-  i, j, arrobas: int8;
+  i, j, caracteres: int8;
   secuencia: string;
 begin
   secuencia:='';
   secuencia:=leer_secuencia(secuencia);
-  i:=1; arrobas:=0;
-  while (secuencia[i]<>'%') do
+  caracteres:=0;
+  i:=1;
+  while (secuencia[i]<>caracter_corte1) do
   begin
-    cumple_A:=cumple_A and (secuencia[i]<>'$');
-    cumple_AB:=cumple_AB and (secuencia[i]<>'$');
+    cumple_A:=cumple_A and (secuencia[i]<>caracter_corte3);
     i:=i+1;
   end;
   j:=i+1;
-  while (secuencia[j]<>'*') do
+  while (secuencia[j]<>caracter_corte2) do
   begin
-    if (secuencia[j]='@') then
-      arrobas:=arrobas+1;
-    cumple_B:=cumple_B and (arrobas<=arrobas_corte);
-    cumple_AB:=cumple_AB and (arrobas<=arrobas_corte);
+    if (secuencia[j]=caracter_corte4) then
+      caracteres:=caracteres+1;
+    cumple_B:=cumple_B and (caracteres<=cantidad_caracter_corte4);
     j:=j+1;
   end;
   cumple_B:=cumple_B and (j/2=i);
-  cumple_AB:=cumple_AB and (j/2=i);
 end;
 var
-  cumple_A, cumple_B, cumple_AB: boolean;
+  cumple_A, cumple_B: boolean;
 begin
-  cumple_A:=true; cumple_B:=true; cumple_AB:=true;
-  parseo_string(cumple_A,cumple_B,cumple_AB);
-  if (cumple_AB=true) then
+  cumple_A:=true; cumple_B:=true;
+  parseo_string(cumple_A,cumple_B);
+  if ((cumple_A=true) and (cumple_B=true)) then
   begin
-    textcolor(yellow); write('La secuencia cumple con el patrón A%B*');
+    textcolor(red); write('La secuencia cumple con el patrón A%B*');
   end
   else if ((cumple_A=false) and (cumple_B=true)) then
   begin
-    textcolor(yellow); write('La secuencia no cumple con la parte A del patrón A%B*');
+    textcolor(red); write('La secuencia no cumple con la parte A del patrón A%B*');
   end
   else if ((cumple_A=true) and (cumple_B=false)) then
   begin
-    textcolor(yellow); write('La secuencia no cumple con la parte B del patrón A%B*');
+    textcolor(red); write('La secuencia no cumple con la parte B del patrón A%B*');
   end
   else
   begin
-    textcolor(yellow); write('La secuencia no cumple con las partes A y B del patrón A%B*');
+    textcolor(red); write('La secuencia no cumple con las partes A y B del patrón A%B*');
   end;
-end.
+end.}
