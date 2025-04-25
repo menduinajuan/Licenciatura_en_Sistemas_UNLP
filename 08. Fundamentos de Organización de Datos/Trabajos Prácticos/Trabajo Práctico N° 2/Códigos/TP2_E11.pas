766524b904ecb7ddb9b50cf3aea479198bcb6342
menduinajuan <menduinajuan@gmail.com>
2025-04-25 20:01:44 -0300

    Commit

--- 08. Fundamentos de Organización de Datos/Trabajos Prácticos/Trabajo Práctico N° 2/Códigos/TP2_E11.pas
+++ 08. Fundamentos de Organización de Datos/Trabajos Prácticos/Trabajo Práctico N° 2/Códigos/TP2_E11.pas
@@ -11,18 +11,141 @@ program TP2_E11;
 {$codepage UTF8}
 uses crt;
 const
-  categoría_ini=1; categoria_fin=15;
+  departamento_salida=999;
+  categoria_ini=1; categoria_fin=15;
 type
-  t_categoria=categoría_ini..categoria_fin;
-  t_registro_horas=record
+  t_categoria=categoria_ini..categoria_fin;
+  t_registro_empleado=record
     departamento: int16;
     division: int16;
     empleado: int16;
     categoria: t_categoria;
     horas: int16;
   end;
+  t_vector_horas=array[t_categoria] of real;
+  t_archivo_maestro=file of t_registro_empleado;
+procedure cargar_vector_horas(var vector_horas: t_vector_horas; var archivo_carga_vector: text);
 var
-
+  categoria: t_categoria;
+  valor_hora: real;
 begin
-  
+  reset(archivo_carga_vector);
+  while (not eof(archivo_carga_vector)) do
+  begin
+    readln(archivo_carga_vector,categoria,valor_hora);
+    vector_horas[categoria]:=valor_hora;
+  end;
+  close(archivo_carga_vector);
+end;
+procedure imprimir_vector_horas(vector_horas: t_vector_horas);
+var
+  i: t_categoria;
+begin
+  for i:= categoria_ini to categoria_fin do
+  begin
+    textcolor(green); write('El valor de la hora de la categoría ',i,' es '); textcolor(yellow); writeln(vector_horas[i]:0:2);
+  end;
+end;
+procedure cargar_archivo_maestro(var archivo_maestro: t_archivo_maestro; var archivo_carga_maestro: text);
+var
+  registro_empleado: t_registro_empleado;
+begin
+  rewrite(archivo_maestro);
+  reset(archivo_carga_maestro);
+  while (not eof(archivo_carga_maestro)) do
+  begin
+    with registro_empleado do
+    begin
+      readln(archivo_carga_maestro,departamento,division,empleado,categoria,horas);
+      write(archivo_maestro,registro_empleado);
+    end;
+  end;
+  close(archivo_maestro);
+  close(archivo_carga_maestro);
+end;
+procedure imprimir_registro_empleado(registro_empleado: t_registro_empleado);
+begin
+  textcolor(green); write('Departamento: '); textcolor(yellow); write(registro_empleado.departamento);
+  textcolor(green); write('; División: '); textcolor(yellow); write(registro_empleado.division);
+  textcolor(green); write('; Número de empleado: '); textcolor(yellow); write(registro_empleado.empleado);
+  textcolor(green); write('; Categoría: '); textcolor(yellow); write(registro_empleado.categoria);
+  textcolor(green); write('; Horas extras: '); textcolor(yellow); writeln(registro_empleado.horas);
+end;
+procedure imprimir_archivo_maestro(var archivo_maestro: t_archivo_maestro);
+var
+  registro_empleado: t_registro_empleado;
+begin
+  reset(archivo_maestro);
+  while (not eof(archivo_maestro)) do
+  begin
+    read(archivo_maestro,registro_empleado);
+    imprimir_registro_empleado(registro_empleado);
+  end;
+  close(archivo_maestro);
+end;
+procedure leer_empleado(var archivo_maestro: t_archivo_maestro; var registro_empleado: t_registro_empleado);
+begin
+  if (not eof(archivo_maestro)) then
+    read(archivo_maestro,registro_empleado)
+  else
+    registro_empleado.departamento:=departamento_salida;
+end;
+procedure procesar_archivo_maestro(var archivo_maestro: t_archivo_maestro; vector_horas: t_vector_horas);
+var
+  registro_empleado: t_registro_empleado;
+  categoria: t_categoria;
+  departamento, division, empleado, horas_departamento, horas_division, horas_empleado: int16;
+  monto_departamento, monto_division, monto_empleado: real;
+begin
+  reset(archivo_maestro);
+  leer_empleado(archivo_maestro,registro_empleado);
+  while (registro_empleado.departamento<>departamento_salida) do
+  begin
+    departamento:=registro_empleado.departamento;
+    horas_departamento:=0; monto_departamento:=0;
+    textcolor(green); write('Departamento: '); textcolor(yellow); writeln(departamento);
+    while (registro_empleado.departamento=departamento) do
+    begin
+      division:=registro_empleado.division;
+      horas_division:=0; monto_division:=0;
+      textcolor(green); write('División: '); textcolor(yellow); writeln(division);
+      textcolor(green); writeln('Número de Empleado          Total de Hs.          Importe a cobrar');
+      while ((registro_empleado.departamento=departamento) and (registro_empleado.division=division)) do
+      begin
+        empleado:=registro_empleado.empleado; categoria:=registro_empleado.categoria;
+        horas_empleado:=0; monto_empleado:=0;
+        while ((registro_empleado.departamento=departamento) and (registro_empleado.division=division) and (registro_empleado.empleado=empleado)) do
+        begin
+          horas_empleado:=horas_empleado+registro_empleado.horas;
+          leer_empleado(archivo_maestro,registro_empleado);
+        end;
+        monto_empleado:=horas_empleado*vector_horas[categoria];
+        textcolor(yellow); write(empleado); textcolor(green); write('                           '); textcolor(red); write(horas_empleado); textcolor(green); write('                    $'); textcolor(red); writeln(monto_empleado:0:2);
+        horas_division:=horas_division+horas_empleado;
+        monto_division:=monto_division+monto_empleado;
+      end;
+      textcolor(green); write('Total horas división: '); textcolor(red); writeln(horas_division);
+      textcolor(green); write('Monto total división: $'); textcolor(red); writeln(monto_division:0:2);
+      horas_departamento:=horas_departamento+horas_division;
+      monto_departamento:=monto_departamento+monto_division;
+    end;
+    textcolor(green); write('Total horas departamento: '); textcolor(red); writeln(horas_departamento);
+    textcolor(green); write('Monto total departamento: $'); textcolor(red); writeln(monto_departamento:0:2); writeln();
+  end;
+end;
+var
+  vector_horas: t_vector_horas;
+  archivo_maestro: t_archivo_maestro;
+  archivo_carga_vector, archivo_carga_maestro: text;
+begin
+  writeln(); textcolor(red); writeln('IMPRESIÓN VECTOR HORAS:'); writeln();
+  assign(archivo_carga_vector,'E11_horasVector.txt');
+  cargar_vector_horas(vector_horas,archivo_carga_vector);
+  imprimir_vector_horas(vector_horas);
+  writeln(); textcolor(red); writeln('IMPRESIÓN ARCHIVO MAESTRO:'); writeln();
+  assign(archivo_maestro,'E11_empleadosMaestro'); assign(archivo_carga_maestro,'E11_empleadosMaestro.txt');
+  cargar_archivo_maestro(archivo_maestro,archivo_carga_maestro);
+  imprimir_archivo_maestro(archivo_maestro);
+  writeln(); textcolor(red); writeln('PROCESAMIENTO ARCHIVO MAESTRO:'); writeln();
+  procesar_archivo_maestro(archivo_maestro,vector_horas);
 end.
--- 08. Fundamentos de Organización de Datos/Trabajos Prácticos/Trabajo Práctico N° 2/Códigos/TP2_E12.pas
+++ 08. Fundamentos de Organización de Datos/Trabajos Prácticos/Trabajo Práctico N° 2/Códigos/TP2_E12.pas
@@ -16,11 +16,129 @@ program TP2_E12;
 {$codepage UTF8}
 uses crt;
 const
-  
+  usuario_salida=999;
+  anio_ini=2020; anio_fin=2025;
 type
-
+  t_anio=anio_ini..anio_fin;
+  t_registro_fecha=record
+    anio: int16;
+    mes: int8;
+    dia: int8;
+  end;
+  t_registro_usuario=record
+    fecha: t_registro_fecha;
+    usuario: int16;
+    tiempo: real;
+  end;
+  t_archivo_maestro=file of t_registro_usuario;
+procedure cargar_archivo_maestro(var archivo_maestro: t_archivo_maestro; var archivo_carga_maestro: text);
 var
-
+  registro_usuario: t_registro_usuario;
 begin
-  
+  rewrite(archivo_maestro);
+  reset(archivo_carga_maestro);
+  while (not eof(archivo_carga_maestro)) do
+    with registro_usuario do
+    begin
+      readln(archivo_carga_maestro,fecha.anio,fecha.mes,fecha.dia,usuario,tiempo);
+      write(archivo_maestro,registro_usuario);
+    end;
+  close(archivo_maestro);
+  close(archivo_carga_maestro);
+end;
+procedure imprimir_registro_fecha(registro_fecha: t_registro_fecha);
+begin
+  textcolor(green); write('Fecha: '); textcolor(yellow); write(registro_fecha.anio);
+  textcolor(green); write('/'); textcolor(yellow); write(registro_fecha.mes);
+  textcolor(green); write('/'); textcolor(yellow); write(registro_fecha.dia);
+end;
+procedure imprimir_registro_usuario(registro_usuario: t_registro_usuario);
+begin
+  imprimir_registro_fecha(registro_usuario.fecha);
+  textcolor(green); write('; ID usuario: '); textcolor(yellow); write(registro_usuario.usuario);
+  textcolor(green); write('; Tiempo de acceso: '); textcolor(yellow); writeln(registro_usuario.tiempo:0:2);
+end;
+procedure imprimir_archivo_maestro(var archivo_maestro: t_archivo_maestro);
+var
+  registro_usuario: t_registro_usuario;
+begin
+  reset(archivo_maestro);
+  while (not eof(archivo_maestro)) do
+  begin
+    read(archivo_maestro,registro_usuario);
+    imprimir_registro_usuario(registro_usuario);
+  end;
+  close(archivo_maestro);
+end;
+procedure leer_acceso(var archivo_maestro: t_archivo_maestro; var registro_usuario: t_registro_usuario);
+begin
+  if (not eof(archivo_maestro)) then
+    read(archivo_maestro,registro_usuario)
+  else
+    registro_usuario.usuario:=usuario_salida;
+end;
+procedure procesar_archivo_maestro(var archivo_maestro: t_archivo_maestro; anio: t_anio);
+var
+  registro_usuario: t_registro_usuario;
+  mes, dia, usuario: int16;
+  tiempo_anio, tiempo_mes, tiempo_dia, tiempo_usuario: real;
+begin
+  reset(archivo_maestro);
+  leer_acceso(archivo_maestro,registro_usuario);
+  while ((registro_usuario.usuario<>usuario_salida) and (registro_usuario.fecha.anio<>anio)) do
+    leer_acceso(archivo_maestro,registro_usuario);
+  if (registro_usuario.usuario<>usuario_salida) then
+  begin
+    tiempo_anio:=0;
+    textcolor(green); write('Año: '); textcolor(yellow); writeln(anio); writeln();
+    while (registro_usuario.fecha.anio=anio) do
+    begin
+      mes:=registro_usuario.fecha.mes;
+      tiempo_mes:=0;
+      textcolor(green); write('  Mes: '); textcolor(yellow); writeln(mes);
+      while ((registro_usuario.fecha.anio=anio) and (registro_usuario.fecha.mes=mes)) do
+      begin
+        dia:=registro_usuario.fecha.dia;
+        tiempo_dia:=0;
+        textcolor(green); write('    Día: '); textcolor(yellow); writeln(dia);
+        textcolor(green); write('      idUsuario          Tiempo Total de acceso en el día '); textcolor(yellow); write(dia); textcolor(green); write(' mes '); textcolor(yellow); writeln(mes);
+        while ((registro_usuario.fecha.anio=anio) and (registro_usuario.fecha.mes=mes) and (registro_usuario.fecha.dia=dia)) do
+        begin
+          usuario:=registro_usuario.usuario;
+          tiempo_usuario:=0;
+          while ((registro_usuario.fecha.anio=anio) and (registro_usuario.fecha.mes=mes) and (registro_usuario.fecha.dia=dia) and (registro_usuario.usuario=usuario)) do
+          begin
+            tiempo_usuario:=tiempo_usuario+registro_usuario.tiempo;
+            leer_acceso(archivo_maestro,registro_usuario);
+          end;
+          textcolor(green); write('      '); textcolor(yellow); write(usuario); textcolor(green); write('                  '); textcolor(red); writeln(tiempo_usuario:0:2);
+          tiempo_dia:=tiempo_dia+tiempo_usuario;
+        end;
+        textcolor(green); write('    Tiempo total acceso día '); textcolor(yellow); write(dia); textcolor(green); write(' mes '); textcolor(yellow); write(mes); textcolor(green); write(': '); textcolor(red); writeln(tiempo_dia:0:2);
+        tiempo_mes:=tiempo_mes+tiempo_dia;
+      end;
+      textcolor(green); write('  Tiempo total acceso mes '); textcolor(yellow); write(mes); textcolor(green); write(': '); textcolor(red); writeln(tiempo_mes:0:2); writeln();
+      tiempo_anio:=tiempo_anio+tiempo_mes;
+    end;
+    textcolor(green); write('Tiempo total acceso año '); textcolor(yellow); write(anio); textcolor(green); write(': '); textcolor(red); writeln(tiempo_anio:0:2);
+  end
+  else
+  begin
+    textcolor(green); write('Año '); textcolor(yellow); write(anio); textcolor(green); writeln(' no encontrado');
+  end;
+  close(archivo_maestro);
+end;
+var
+  archivo_maestro: t_archivo_maestro;
+  archivo_carga_maestro: text;
+  anio: t_anio;
+begin
+  randomize;
+  writeln(); textcolor(red); writeln('IMPRESIÓN ARCHIVO MAESTRO:'); writeln();
+  assign(archivo_maestro,'E12_usuariosMaestro'); assign(archivo_carga_maestro,'E12_usuariosMaestro.txt');
+  cargar_archivo_maestro(archivo_maestro,archivo_carga_maestro);
+  imprimir_archivo_maestro(archivo_maestro);
+  writeln(); textcolor(red); writeln('PROCESAMIENTO ARCHIVO MAESTRO:'); writeln();
+  anio:=anio_ini+random(anio_fin-anio_ini+1);
+  procesar_archivo_maestro(archivo_maestro,anio);
 end.
