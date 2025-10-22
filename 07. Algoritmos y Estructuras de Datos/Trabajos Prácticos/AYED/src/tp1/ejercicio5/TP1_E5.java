c273c721859a3a318dc6d90b7735c8b70b58ef95
menduinajuan <menduinajuan@gmail.com>
2025-10-22 20:02:44 -0300

    Commit

--- 07. Algoritmos y Estructuras de Datos/Trabajos Prácticos/AYED/src/tp1/ejercicio5/TP1_E5.java
+++ 07. Algoritmos y Estructuras de Datos/Trabajos Prácticos/AYED/src/tp1/ejercicio5/TP1_E5.java
@@ -2,10 +2,10 @@
 /*EJERCICIO 5*/
 /*
 Dado un arreglo de valores tipo entero, se desea calcular el valor máximo, mínimo y promedio en un único método.
-Escribir tres métodos de clase, donde respectivamente:
-(a) Devolver lo pedido por el mecanismo de retorno de un método en Java (“return”).
-(b) Devolver lo pedido interactuando con algún parámetro (el parámetro no puede ser de tipo arreglo).
-(c) Devolver lo pedido sin usar parámetros ni la sentencia “return”.
+Escribir tres métodos de clase, donde, respectivamente:
+(a) Devuelva lo pedido por el mecanismo de retorno de un método en Java (“return”).
+(b) Devuelva lo pedido interactuando con algún parámetro (el parámetro no puede ser de tipo arreglo).
+(c) Devuelva lo pedido sin usar parámetros ni la sentencia “return”.
 */
 
 package tp1.ejercicio5;
